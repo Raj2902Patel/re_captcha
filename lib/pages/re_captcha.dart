086a635d5635159281ea8c16767e4c062a6e19a9
Raj2902Patel <rajpatel.imscit20@gmail.com>
2024-10-15 17:14:41 +0530

    Work on Speaking Functionality

--- lib/pages/re_captcha.dart
+++ lib/pages/re_captcha.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:lottie/lottie.dart';
+
 class ReCaptchaPage extends StatefulWidget {
   const ReCaptchaPage({super.key});
 
@@ -9,6 +12,41 @@ class ReCaptchaPage extends StatefulWidget {
 }
 
 class _ReCaptchaPageState extends State<ReCaptchaPage> {
+  final FlutterTts flutterTts = FlutterTts();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool isSpeaking = false; // Track speaking status
+
+  speak(String text) async {
+    setState(() {
+      isSpeaking = true; // Set speaking status to true
+    });
+
+    await flutterTts.setLanguage("en-US");
+
+    for (int i = 0; i < text.length; i++) {
+      String character = text[i];
+
+      // Check if the character is uppercase
+      if (character == character.toUpperCase() && character != ' ') {
+        await flutterTts.setPitch(1.5); // Set a higher pitch for uppercase
+        await flutterTts.setSpeechRate(
+            0.7); // Set a slightly faster speech rate for uppercase
+      } else {
+        await flutterTts.setPitch(1.0); // Normal pitch for lowercase
+        await flutterTts.setSpeechRate(0.5); // Normal speech rate for lowercase
+      }
+
+      await flutterTts.speak(character);
+      await Future.delayed(
+          const Duration(seconds: 1)); // Slight delay between characters
+    }
+
+    // After speaking is done, set speaking status to false
+    setState(() {
+      isSpeaking = false;
+    });
+  }
+
   String randomString = "";
   String verificationText = "";
   bool isVerified = false;
@@ -18,7 +56,6 @@ class _ReCaptchaPageState extends State<ReCaptchaPage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     buildCaptcha();
   }
@@ -42,137 +79,169 @@ class _ReCaptchaPageState extends State<ReCaptchaPage> {
         ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(12.0),
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 2.0),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: Text(
-                    randomString,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                      letterSpacing: 3.0,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(12.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2.0),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Text(
+                      randomString,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22.0,
+                        letterSpacing: 3.0,
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                IconButton(
-                  onPressed: () {
+                  const SizedBox(width: 10),
+                  IconButton(
+                    onPressed: isSpeaking
+                        ? null
+                        : () => setState(() {
+                              buildCaptcha();
+                            }),
+                    icon: isSpeaking
+                        ? Icon(Icons.block)
+                        : const Icon(
+                            Icons.refresh_outlined,
+                            size: 30.0,
+                          ),
+                  ),
+                  const SizedBox(width: 10),
+                  IconButton(
+                    onPressed: isSpeaking
+                        ? null
+                        : () => speak(randomString), // Disable if speaking
+                    icon: isSpeaking
+                        ? const Icon(Icons.volume_off_outlined)
+                        : const Icon(Icons.volume_up_outlined),
+                  ),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.all(25.0),
+                child: TextFormField(
+                  onChanged: (value) {
                     setState(() {
-                      buildCaptcha();
+                      isVerified = false;
                     });
+                    _formKey.currentState?.validate(); // Dynamically validate
                   },
-                  icon: const Icon(
-                    Icons.refresh_outlined,
-                    size: 30.0,
+                  decoration: InputDecoration(
+                    errorStyle: const TextStyle(fontSize: 15.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide: const BorderSide(
+                        color: Colors.black,
+                        width: 1.5,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide: const BorderSide(
+                        color: Colors.blueGrey,
+                        width: 1.5,
+                      ),
+                    ),
+                    hintText: "Enter Captcha",
                   ),
-                )
-              ],
-            ),
-            Container(
-              decoration: const BoxDecoration(),
-              margin: const EdgeInsets.all(10.0),
-              child: TextFormField(
-                onChanged: (value) {
-                  setState(() {
-                    isVerified = false;
-                  });
+                  controller: captchaController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Captcha is required';
+                    } else if (value != randomString) {
+                      return 'Incorrect Captcha';
+                    }
+                    return null; // No error if captcha is correct
+                  },
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black.withOpacity(0.6),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // If form is valid, verify captcha
+                    setState(() {
+                      verificationText = "Verified!";
+                      showVerificationIcon = true;
+                      captchaController.clear();
+                    });
+
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: LottieBuilder.asset(
+                          'assets/verified.json',
+                          height: 120,
+                        ),
+                        content: Container(
+                          child: const Text(
+                            textAlign: TextAlign.center,
+                            "reCaptcha Verified!",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                        actions: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black.withOpacity(0.5),
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                buildCaptcha();
+                                captchaController.clear();
+                                verificationText = "";
+                                isVerified = false;
+                              });
+                              Navigator.pop(context);
+                            },
+                            child: const Text(
+                              "OK",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  } else {
+                    setState(() {
+                      verificationText = "Please Enter Correct Characters!";
+                      showVerificationIcon = false;
+                    });
+                  }
                 },
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0),
+                child: const Text(
+                  "Save",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.w400,
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                    borderSide: const BorderSide(
-                      color: Colors.black,
-                      width: 1.5,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                    borderSide: const BorderSide(
-                      color: Colors.blueGrey,
-                      width: 1.5,
-                    ),
-                  ),
-                  hintText: "Enter Captcha",
-                ),
-                controller: captchaController,
-              ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Visibility(
-                  visible: showVerificationIcon,
-                  child: const Icon(Icons.verified),
-                ),
-                const SizedBox(
-                  width: 5,
-                ),
-                isVerified
-                    ? Text(
-                        verificationText,
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      )
-                    : Text(
-                        verificationText,
-                        style: const TextStyle(
-                          color: Colors.red,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      )
-              ],
-            ),
-            const SizedBox(
-              height: 15.0,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black.withOpacity(0.6),
-              ),
-              onPressed: () {
-                isVerified = captchaController.text == randomString;
-
-                if (isVerified) {
-                  verificationText = "Verified!";
-                  showVerificationIcon = true;
-                } else {
-                  verificationText = "Please Enter Correct Characters!";
-                  showVerificationIcon = false;
-                }
-
-                setState(() {});
-              },
-              child: const Text(
-                "Save",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.w400,
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
